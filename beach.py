6dd5a7bc40adf2a676f311814ee05d40582ae87a
Snizhko Pavel <pavel.snizhko.2000@gmail.com>
2020-02-15 13:56:30 +0200

    first commit in drawing beach

---
+++ beach.py
@@ -0,0 +1,37 @@
+from graph import *
+
+def draw_sky(x, y, hight_sky):
+    """ hight_sky + hight_sea + hight_beach = size of window"""
+    pass
+
+
+def dra_cloud(x, y):
+    pass
+
+
+def draw_sun(x, y, radius):
+    pass
+
+
+def draw_sea(x, y, hight_sea):
+    pass
+
+def draw_boat(x, y, size):
+    """ x, y - position rostrum 
+        x + size ends of ship"""
+    pass
+
+
+def draw_beach(x, y, hight_beach):
+    pass
+
+
+def draw_umbrella(x, y, length):
+    pass
+
+
+def picture():
+    pass
+
+if __name__ == '__main__':
+    picture()
